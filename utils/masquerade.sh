01e69d48f7d404c15ea10c0d58058168cfd3a8b0
Tomasz Klim <github@tomaszklim.pl>
2017-03-05 23:26:32 +0100

    rename masquerade script

--- utils/masquerade.sh
+++ utils/masquerade.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "" ]; then
-	echo "usage: $0 <public-interface>"
+	echo "usage: $0 <interface>"
 	exit 1
 elif ! [[ $1 =~ ^[0-9a-zA-Z:-]+$ ]]; then
 	echo "error: parameter $1 not conforming network interface name format"
